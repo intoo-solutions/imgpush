7c9572ef5970a8be3edb8be9700150d8b38eccd5
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-06-10 17:10:17 +0200

    refactor: update metrics rebuilder to use environment variable for thread count

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,7 +3,7 @@
 # This is to prevent logs from not showing up in the docker logs command
 export PYTHONUNBUFFERED=1
 
-if [ "$REBUILD_METRICS" = "true" ]; then
+if [ "$REBUILD_METRICS" = "1" ]; then
     python3 rebuild_metrics.py
     exit 0
 fi
--- app/rebuild_metrics.py
+++ app/rebuild_metrics.py
@@ -12,7 +12,7 @@ from threading import Lock
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
-formatter = logging.Formatter('[%(asctime)s] [Metrics] %(message)s')
+formatter = logging.Formatter("[%(asctime)s] [Metrics] %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -26,6 +26,7 @@ logger.info("Rebuilding from S3 bucket")
 for line in storage.__str__().split("\n"):
     logger.info(line)
 
+
 # The method that will process each object
 def process_object(object, metrics, lock):
     try:
@@ -33,10 +34,12 @@ def process_object(object, metrics, lock):
         size = object["Size"]
 
         # Skip if it's a folder
-        if filename.endswith('/'):
+        if filename.endswith("/"):
             return
 
-        object_info = storage.s3.head_object(Key=filename, Bucket=settings.S3_BUCKET_NAME)
+        object_info = storage.s3.head_object(
+            Key=filename, Bucket=settings.S3_BUCKET_NAME
+        )
 
         mime_type = object_info.get("ContentType", "others")
 
@@ -47,12 +50,13 @@ def process_object(object, metrics, lock):
     except Exception as e:
         logger.error(f"Error processing object {object['Key']}: {str(e)}")
 
+
 metrics = defaultdict(lambda: {"count": 0, "total_size": 0})
 lock = Lock()
 
 # Fetch all objects from the S3 bucket
 all_objects = []
-paginator = storage.s3.get_paginator('list_objects')
+paginator = storage.s3.get_paginator("list_objects")
 start_time = time.time()
 
 logger.info("Fetching all objects from S3 bucket...")
@@ -63,7 +67,9 @@ try:
             page_content = page["Contents"]
             object_count = len(page_content)
 
-            logger.info(f"Fetched {object_count} {'more ' if index > 0 else ''}objects... ({index + 1}/{'?' if object_count == 1000 else index + 1})")
+            logger.info(
+                f"Fetched {object_count} {'more ' if index > 0 else ''}objects... ({index + 1}/{'?' if object_count == 1000 else index + 1})"
+            )
             all_objects.extend(page["Contents"])
         except Exception as e:
             logger.error(f"Error fetching objects: {str(e)}")
@@ -72,20 +78,33 @@ except Exception as e:
     exit(1)
 
 try:
-    thread_count = int(os.getenv('THREAD_COUNT', 4))
+    thread_count = int(os.getenv("S3_METRICS_REBUILDER_THREAD_COUNT", 4))
 except ValueError:
-    logger.error("THREAD_COUNT environment variable is not a valid integer, defaulting to 4")
+    logger.error(
+        "THREAD_COUNT environment variable is not a valid integer, defaulting to 4"
+    )
     thread_count = 4
 
-logger.info(f"Got {len(all_objects)} objects to process. Starting {thread_count} threads!")
+logger.info(
+    f"Got {len(all_objects)} objects to process. Starting {thread_count} threads!"
+)
 
 # Process the objects
 last_time = time.time()
 with concurrent.futures.ThreadPoolExecutor(thread_count) as executor:
-    for i, _ in enumerate(executor.map(process_object, all_objects, [metrics]*len(all_objects), [lock]*len(all_objects))):
+    for i, _ in enumerate(
+        executor.map(
+            process_object,
+            all_objects,
+            [metrics] * len(all_objects),
+            [lock] * len(all_objects),
+        )
+    ):
         now = time.time()
         if now - last_time > 5:
-            logger.info(f'Progress: {i}/{len(all_objects)} ({i / len(all_objects) * 100:.2f}%)')
+            logger.info(
+                f"Progress: {i}/{len(all_objects)} ({i / len(all_objects) * 100:.2f}%)"
+            )
             last_time = now
 
 end_time = time.time()
@@ -98,7 +117,9 @@ average_time = total_time / len(all_objects) if len(all_objects) > 0 else 0
 metrics["last_execution_time_in_milliseconds"] = int(total_time * 1000)
 metrics["last_execution_date"] = datetime.datetime.now().isoformat()
 
-logger.info(f"Metrics rebuilt in {total_time:.2f}s (average of {average_time:.2f}s per object)")
+logger.info(
+    f"Metrics rebuilt in {total_time:.2f}s (average of {average_time:.2f}s per object)"
+)
 logger.info("Saving file metrics.json...")
 
 import json
